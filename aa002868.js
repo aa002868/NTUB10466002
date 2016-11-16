e4424185f76342b5cf50e8d4aa2ee1af0d98e585
aa002868 <aa002868@au.edu.tw>
2016-11-16 10:38:14 +0800

    jstest

---
+++ aa002868.js
@@ -0,0 +1,46 @@
+function aa002868(){
+var width  = 1024;
+var height = 768; 
+d3.csv("https://aa002868.github.io/NTUB10466002/table.csv", function(data) {
+	console.log(data);
+	data.forEach(function(d) {
+		console.log(d.Open);
+		console.log(d.Low);
+		console.log(d.Close);
+	});
+	var maxy = d3.max(data, function(d) { return d.Open; });
+	var hmaxy = d3.max(data, function(d) { return d.High; });
+	var lmaxy = d3.max(data, function(d) { return d.Low; });
+	var cmaxy = d3.max(data, function(d) { return d.Close; });
+	var ln = data.length;
+	var ctrl  = d3.select(".smallBox").append("svg").attr("width", width).attr("height", height);
+	var lines = d3.line().
+	x(function(d,i){ return i * (width/ln); }).
+	y(function(d){ return height-d.Open * (height/hmaxy); });
+	ctrl.append("path").data([data]).
+	attr("class", "pathline").
+	attr("d", lines);
+	var line2 = d3.line().
+	x(function(d,i){ return i * (width/ln); }).
+	y(function(d){ return height-d.High * (height/hmaxy); });
+	ctrl.append("path").data([data]).
+	attr("class", "pathline").
+	attr("id", "pathline2").
+	attr("d", line2);
+    	var line3 = d3.line().
+	x(function(d,i){ return i * (width/ln); }).
+	y(function(d){ return height-d.Low * (height/hmaxy); });
+	ctrl.append("path").data([data]).
+	attr("class", "pathline").
+	attr("id", "pathline3").
+	attr("d", line3);
+	var line4 = d3.line().
+	x(function(d,i){ return i * (width/ln); }).
+	y(function(d){ return height-d.Close * (height/hmaxy); });
+	ctrl.append("path").data([data]).
+	attr("class", "pathline").
+	attr("id", "pathline4").
+	attr("d", line4);
+	
+});
+}
