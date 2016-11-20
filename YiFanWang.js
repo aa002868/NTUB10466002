6c4f857e991380ef392e34e66c263a3fbd3aae11
aa002868 <aa002868@au.edu.tw>
2016-11-21 00:56:06 +0800

    Update YiFanWang.js

--- YiFanWang.js
+++ YiFanWang.js
@@ -1,9 +1,5 @@
 function YiFanWang(){
-<script src="d3.js"></script>
-<script src="d3.layout.cloud.js"></script>
-<script src="d3.wordcloud.js"></script>
-var width  = 460;
-var height = 227; 
+
 dataPath = 'https://aa002868.github.io/NTUB10466002/';
 	dataFile = 'YiFanWang.csv';
 	dataUrl = dataPath + dataFile;
